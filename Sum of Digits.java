5adbd36cbaecc0722044b69bb3cce220edf3a012
Fahad Salim <85647102+fahadsalim02@users.noreply.github.com>
2022-02-06 23:25:26 +0530

    Create Sum of Digits.java

---
+++ Sum of Digits.java
@@ -0,0 +1,19 @@
+package practice;
+import java.util.Scanner;
+public class SumOfDigitsOfNumbers {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int num = sc.nextInt();
+		int sum = 0;
+		while(num>0) {
+			int rem = num%10;
+			sum = rem + sum;
+			num = num/10;
+		}
+		System.out.println("Sum of the digits = " + sum);
+		sc.close();
+	}
+
+}
